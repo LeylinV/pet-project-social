f80437bf53b98f3f3a3415869df70936469871c0
Damir Rakipov <anas.rakipov@mail.ru>
2023-07-09 17:45:01 +0500

    feat: Обработана ситуация если посты не найдены

--- src/Pages/PagePosts/PostsList/PostsList.jsx
+++ src/Pages/PagePosts/PostsList/PostsList.jsx
@@ -6,7 +6,7 @@ const PostsList = ({posts, isLoading, deletePost}) => {
     return (
         <div>
             <div className='text-center'>
-                <h3 className={`${styles.mainTitle}`}>Посты</h3>
+                <h3 className={styles.mainTitle}>Посты</h3>
             </div>
             <div>
                 {isLoading
@@ -16,9 +16,15 @@ const PostsList = ({posts, isLoading, deletePost}) => {
                             <span className="visually-hidden">Loading...</span>
                         </div>
                     </div>)
-                    : (posts.map((post, index)=>{
-                        return <SinglePost post={post} postNumber={index+1} deletePost={deletePost} key={index} />
-                    }))}
+                    :
+                    (posts.length > 0)
+                        ?
+                        (posts.map((post, index)=>{
+                            return <SinglePost post={post} postNumber={index+1} deletePost={deletePost} key={index} />
+                        }))
+                        :
+                        <h4 className={styles.titleNoPosts}>Посты не найдены!</h4>
+                }
             </div>
         </div>
     );
