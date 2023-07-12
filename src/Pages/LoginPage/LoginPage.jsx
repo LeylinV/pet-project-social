551fb949e4714f4d98458d47d98d25f6d46a65ef
Damir Rakipov <anas.rakipov@mail.ru>
2023-07-12 22:52:48 +0500

    feat: Валидация логина и пароля

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './LoginPage.module.scss'
 
 const LoginPage = () => {
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [loginValid, setLoginValid] = useState()
+    const [passwordValid, setPasswordValid] = useState()
+
+    const tryAuth = () => {
+        setLoginValid(login ? 'is-valid': 'is-invalid')
+        setPasswordValid(password.length > 8 ? 'is-valid': 'is-invalid')
+    }
+
     return (
         <div className="container mt-3">
             <div className={styles.loginBlock}>
                 <h3 className="mb-5">Логин</h3>
                 <div className="form-floating mb-5">
-                    <input type="text" className="form-control" id="floatingInput" placeholder="Логин"/>
+                    <input
+                        value={login}
+                        onChange={(e)=>setLogin(e.target.value)}
+                        type="text"
+                        className={`form-control ` + loginValid}
+                        id="floatingInput"
+                        placeholder="Логин"
+                    />
                     <label htmlFor="floatingInput">Логин</label>
+                    <div className="invalid-feedback">
+                        Введите логин
+                    </div>
                 </div>
                 <div className="form-floating mb-5">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Пароль"/>
+                    <input
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                        type="password"
+                        className={`form-control ` + passwordValid}
+                        id="floatingPassword"
+                        placeholder="Пароль"
+                    />
                     <label htmlFor="floatingPassword">Пароль</label>
+                    <div className="invalid-feedback">
+                        Пароль должен быть длинной от 8 символов
+                    </div>
                 </div>
-                <div className="d-grid gap-2 col-6 mx-auto">
+                <div onClick={tryAuth} className="d-grid gap-2 col-6 mx-auto">
                     <button className="btn btn-primary btn-lg" type="button">Войти</button>
                 </div>
             </div>
