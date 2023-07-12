e5b372dde56c4fed1d040987b9ab273795bbbadf
Damir Rakipov <anas.rakipov@mail.ru>
2023-07-12 20:51:25 +0500

    feat: Переработан fetching поста и комментариев

--- src/Pages/PageSinglePost/PageSinglePost.jsx
+++ src/Pages/PageSinglePost/PageSinglePost.jsx
@@ -3,42 +3,38 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import styles from './PageSinglePost.module.scss'
 import CommentsForPost from './CommentsForPost/CommentsForPost';
+import PostService from "../../API/PostService";
+import {useFetching} from "../../hooks/useFetching";
 
 const PageSinglePost = () => {
     const {id} = useParams()
     const [post, setPost] = useState(null)
-    const [isLoadingPost, setIsLoadingPost] = useState(true)
     const [comments, setComments] = useState([])
-    const [isLoadingComments, setIsLoadingComments] = useState(true)
-    useEffect(()=>{
-        setIsLoadingPost(true)
-        setIsLoadingComments(true)
 
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            .then(res=> {
-                setPost(res.data)
-                setIsLoadingPost(false)
+    const [fetchPostById, isLoadingPost, postError] = useFetching(async () => {
+        const response = await PostService.getById(id);
+        setPost(response.data)
+    })
 
-                return axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${res.data.id}`)
-            })
-            .then(res=>{
-                setComments(res.data)
-                setIsLoadingComments(false)
-            })
-            .catch(()=>{
-                setIsLoadingPost(false)
-                setIsLoadingComments(false)
-                setPost(
-                    {
-                        title:'Пост не найден!',
-                        body: 'Скорее всего, вы открыли пост, который создали сами и он не был найден по запросу',
-                        id: id
-                    })
-            })
-    },[id])
+    const [fetchCommentsById, isLoadingComments, commentsError] = useFetching(async () => {
+        const response = await PostService.getCommentsByPostId(id);
+        setComments(response.data)
+    })
+
+    useEffect(() => {
+        fetchPostById()
+        fetchCommentsById()
+    }, [id])
+
+
+
+
     return (
         <div className='container mt-3'>
             <div>
+                {postError &&
+                    <h1>Произошла ошибка. Скорее всего, вы открыли пост, который создали сами и он не был найден по запросу</h1>
+                }
                 {isLoadingPost
                     ? (
                     <div className="d-flex justify-content-center">
@@ -49,11 +45,11 @@ const PageSinglePost = () => {
                     :   <div>
                             <div>
                                 <div className='text-center'>
-                                    <h3 className={styles.mainTitle}>Пост с ID={post.id}</h3>
-                                    <h3 className={styles.secondTitle}>{post.title}</h3>
+                                    <h3 className={styles.mainTitle}>Пост с ID={post?.id}</h3>
+                                    <h3 className={styles.secondTitle}>{post?.title}</h3>
                                 </div>
                                 <div className={styles.postBody}>
-                                    <p>{post.body}</p>
+                                    <p>{post?.body}</p>
                                 </div>
                             </div>
                             <CommentsForPost comments={comments} isLoadingComments={isLoadingComments} />
